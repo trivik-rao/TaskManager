7d8be32d13ecf6379e996d2d8dc3ec7c4f65c0f6
TrivikRao <rao.trivikram@gmail.com>
2026-06-19 23:39:01 -0300

    Add Firebase project credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,10 @@
-// Paste your Firebase project config here (from Firebase Console → Project Settings → Your apps)
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyBZGs_LTQ9i-nbJf9KP6EUMg3Gh-kbS4Mo",
+  authDomain: "taskmanager-34346.firebaseapp.com",
+  projectId: "taskmanager-34346",
+  storageBucket: "taskmanager-34346.firebasestorage.app",
+  messagingSenderId: "589417666743",
+  appId: "1:589417666743:web:e113a0acc0818d7e9a9532"
 };
 
 firebase.initializeApp(firebaseConfig);
